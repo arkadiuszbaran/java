7dd07d16f62cf87072b91a03c02af45d5bcaa1ca
arkadiuszbaran <vertilly@gmail.com>
2020-07-22 22:40:32 +0200

    Rozdział 3.

---
+++ FewInformation/src/FewInformation.java
@@ -0,0 +1,17 @@
+//zad. 6
+//ćw. 1
+public class FewInformation {
+    public static void main(String[] args) {
+        System.out.print("Cześć Arek, słyszałem, że masz ");
+        System.out.print(25);
+        System.out.println(" lat i uczysz się programowania. Czy to prawda?");
+        System.out.println(false);
+        System.out.print("Czy Twoje inicjały to ");
+        System.out.print('A');
+        System.out.print(' ');
+        System.out.print('B');
+        System.out.println('?');
+        System.out.print("Tak, ");
+        System.out.println(true);
+    }
+}
